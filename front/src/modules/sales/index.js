d14c7144e30e80f23f86a24c80b26df413cea3dc
@0780Richie <richard@highcalibervisuals.com>
2020-02-27 04:19:12 +0500

    feat: new action  added to sales redux

--- front/src/modules/sales/index.js
+++ front/src/modules/sales/index.js
@@ -1,30 +1,49 @@
-import _ from 'lodash';
+import _ from "lodash";
 
-const SET_SALES_DATA = 'sale/SET_SALES_DATA'
+const SET_SALES_DATA = "sale/SET_SALES_DATA";
+const SET_SALES_COMPARISONS_DATA = "sale/SET_SALES_COMPARISONS_DATA";
 
-const initialState = {};
+const initialState = {
+  active: {},
+  comparisons: {}
+};
 
 export default (state = initialState, action) => {
-    state = _.cloneDeep(state);
-    switch (action.type) {
-        case SET_SALES_DATA:
-            state = action.data; 
-            break;
-        default:
-            break;
-    }
-    return state;
-}
+  state = _.cloneDeep(state);
+  switch (action.type) {
+    case SET_SALES_DATA:
+      state.active = action.data;
+      break;
+    case SET_SALES_COMPARISONS_DATA:
+      state.comparisons = action.data;
+      break;
 
-const setData = (data) => {
-    return (dispatch) => {
-        dispatch({
-            type: SET_SALES_DATA,
-            data
-        })
-    }
-}
+    default:
+      break;
+  }
+  return state;
+};
+
+const setData = data => {
+  return dispatch => {
+    dispatch({
+      type: SET_SALES_DATA,
+      data
+    });
+  };
+};
+
+const setSecondData = data => {
+    return dispatch => {
+      dispatch({
+        type: SET_SALES_COMPARISONS_DATA,
+        data
+      });
+    };
+  };
+
 
 export const actions = {
-    saleSetData: setData
+  saleSetData: setData,
+  setSecondData
 };
