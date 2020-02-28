053afa51dc36685f946ee463337c2ee2dabb30da
@0780Richie <richard@highcalibervisuals.com>
2020-02-28 22:12:27 +0500

    refactor: improve numberWithCommas formula

--- front/src/services/numberWithCommas/index.js
+++ front/src/services/numberWithCommas/index.js
@@ -1,6 +1,8 @@
 
 const numberWithCommas = (x) => {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    const parts = x.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
 }
 
 export default numberWithCommas;
