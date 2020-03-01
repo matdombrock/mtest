e9fe0f261c78d8bf02962bfaf250287a2d277b74
@0780Richie <richard@highcalibervisuals.com>
2020-03-01 12:09:23 +0500

    feat: #4 SKU redux added

--- front/src/modules/sales/index.js
+++ front/src/modules/sales/index.js
@@ -2,10 +2,13 @@ import _ from "lodash";
 
 const SET_SALES_DATA = "sale/SET_SALES_DATA";
 const SET_SALES_COMPARISONS_DATA = "sale/SET_SALES_COMPARISONS_DATA";
-
+const SET_SKU_DATA = "sale/SET_SKU_DATA";
+const SET_SKU_DATA_COMPARISONS = "sale/SET_SKU_DATA_COMPARISONS";
 const initialState = {
   active: {},
-  comparisons: {}
+  comparisons: {},
+  skuActive: {},
+  skuComparisons: {}
 };
 
 export default (state = initialState, action) => {
@@ -17,7 +20,12 @@ export default (state = initialState, action) => {
     case SET_SALES_COMPARISONS_DATA:
       state.comparisons = action.data;
       break;
-
+    case SET_SKU_DATA:
+      state.skuActive = action.data;
+      break;
+    case SET_SKU_DATA_COMPARISONS:
+      state.skuComparisons = action.data;
+      break;
     default:
       break;
   }
@@ -33,17 +41,44 @@ const setData = data => {
   };
 };
 
-const setSecondData = data => {
-    return dispatch => {
-      dispatch({
-        type: SET_SALES_COMPARISONS_DATA,
-        data
-      });
-    };
+const setSKUData = data => {
+  const total = { totalSale: 0 };
+  data.map(d => {
+    total.totalSale += Number(d.sales);
+  });
+  return dispatch => {
+    dispatch({
+      type: SET_SKU_DATA,
+      data: { total, data }
+    });
   };
+};
 
+const setSKUComprisionData = data => {
+  const total = { totalSale: 0 };
+  data.map(d => {
+    total.totalSale += Number(d.sales);
+  });
+  return dispatch => {
+    dispatch({
+      type: SET_SKU_DATA_COMPARISONS,
+      data: { total, data }
+    });
+  };
+};
+
+const setSecondData = data => {
+  return dispatch => {
+    dispatch({
+      type: SET_SALES_COMPARISONS_DATA,
+      data
+    });
+  };
+};
 
 export const actions = {
   saleSetData: setData,
-  setSecondData
+  setSecondData,
+  setSKUComprisionData,
+  setSKUData
 };
