1562b9f001e0cf4cf74007d47f69fff2b39573e4
@0780Richie <richard@highcalibervisuals.com>
2020-03-07 10:38:55 +0500

    fix: #43

--- front/src/services/numberWithCommas/index.js
+++ front/src/services/numberWithCommas/index.js
@@ -1,8 +1,10 @@
-
-const numberWithCommas = (x) => {
-    const parts = Number(x).toFixed(2).toString().split(".");
-    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    return parts.join(".");
-}
+const numberWithCommas = x => {
+  const selectedNumber = Number(x);
+  const num =
+    selectedNumber % 1 === 0 ? selectedNumber : selectedNumber.toFixed(2);
+  const parts = num.toString().split(".");
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return parts.join(".");
+};
 
 export default numberWithCommas;
