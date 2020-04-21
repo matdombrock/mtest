435faf12ca75e92ea5e6a462bc286bf866a11346
@0780Richie <richard@highcalibervisuals.com>
2020-04-22 04:48:31 +0500

    feat: #87

--- front/src/components/page/importProducts/index.js
+++ front/src/components/page/importProducts/index.js
@@ -5,17 +5,47 @@ import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import TextInput from "@material-ui/core/TextField";
 import s from "./style.module.scss";
-import { fetchNonAdminUsers } from "../../../services/api";
+import { fetchNonAdminUsers, uploadProduct } from "../../../services/api";
 import { actions as usersActions } from "../../../modules/users";
 import Button from "@material-ui/core/Button";
+import Snackbar from "@material-ui/core/Snackbar";
+import Alert from "@material-ui/lab/Alert";
 
 class ImportProducts extends React.Component {
   constructor(props) {
     super(props);
     fetchNonAdminUsers().then((res) => props.usersSetData(res.users));
+    this.state = {
+      file: null,
+      alert: {
+        message: "",
+        type: "",
+        isShown: false,
+      },
+    };
   }
-
+  handleSubmit = async () => {
+    try {
+      const { file } = this.state;
+      if (file) {
+        this._handleAlert(true, "please wait", "warning");
+        const payload = new FormData();
+        payload.append("products_file", file[0]);
+        await uploadProduct(payload);
+        this._handleAlert(true, "Successfully Uploaded", "success");
+      }
+    } catch (error) {
+      this._handleAlert(true, "Something went wrong", "error");
+      console.log("ImportProducts -> handleSubmit -> error", error);
+    }
+  };
+  _handleChangeFile = (e) => this.setState({ file: e.currentTarget.files });
+  _handleAlert = (toggle, message = "", type = "success") =>
+    this.setState((pre) => ({
+      alert: { ...pre.alert, isShown: toggle, message, type },
+    }));
   render() {
+    const { alert } = this.state;
     return (
       <div className={s.container}>
         <Card style={{ marginBottom: "1em" }}>
@@ -27,19 +57,44 @@ class ImportProducts extends React.Component {
               <div className={s.userGridInner}>
                 <Button variant="contained" component="label">
                   Upload File
-                  <input type="file" style={{ display: "none" }} />
+                  <input
+                    type="file"
+                    onChange={this._handleChangeFile}
+                    accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
+                    style={{ display: "none" }}
+                  />
                 </Button>
               </div>
             </Grid>
             <Grid item xs={6} className={s.container}>
               <div className={s.userGridInner}>
-                <Button onClick={null} variant="contained" color="primary">
+                <Button
+                  onClick={this.handleSubmit}
+                  variant="contained"
+                  color="primary"
+                >
                   Upload
                 </Button>
               </div>
             </Grid>
           </Grid>
         </Card>
+        {alert.isShown && (
+          <Snackbar
+            open={alert.isShown}
+            autoHideDuration={1000}
+            onClose={(e) => this._handleAlert(false)}
+          >
+            <Alert
+              onClose={(e) => this._handleAlert(false)}
+              elevation={6}
+              variant="filled"
+              severity={alert.type}
+            >
+              {alert.message}
+            </Alert>
+          </Snackbar>
+        )}
       </div>
     );
   }
--- front/src/services/api/index.js
+++ front/src/services/api/index.js
@@ -8,11 +8,11 @@ export const fetchBrands = () => {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-      "x-access-token": sessionStorage.getItem("moda_token")
-    }
+      "x-access-token": sessionStorage.getItem("moda_token"),
+    },
   })
-    .then(response => response.json())
-    .then(response => response);
+    .then((response) => response.json())
+    .then((response) => response);
 };
 
 export const fetchNonAdminUsers = () => {
@@ -20,69 +20,80 @@ export const fetchNonAdminUsers = () => {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-      "x-access-token": sessionStorage.getItem("moda_token")
-    }
+      "x-access-token": sessionStorage.getItem("moda_token"),
+    },
   })
-    .then(response => response.json())
-    .then(response => response);
+    .then((response) => response.json())
+    .then((response) => response);
 };
 
-export const fetchSalesDataBySKU = data => {
+export const fetchSalesDataBySKU = (data) => {
   let url = config.remoteIP + "/api/getSKUSalesByBrand";
   return fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "x-access-token": sessionStorage.getItem("moda_token")
+      "x-access-token": sessionStorage.getItem("moda_token"),
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
   })
-    .then(async response => ({
+    .then(async (response) => ({
       ...(await response.json()),
-      status: response.status
+      status: response.status,
     }))
-    .then(response => response);
+    .then((response) => response);
 };
 
-export const fetchSalesData = data => {
+export const fetchSalesData = (data) => {
   let url = config.remoteIP + "/api/getSalesByBrand";
   return fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "x-access-token": sessionStorage.getItem("moda_token")
+      "x-access-token": sessionStorage.getItem("moda_token"),
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
   })
-    .then(async response => ({
+    .then(async (response) => ({
       data: await response.json(),
-      status: response.status
+      status: response.status,
     }))
-    .then(response => response);
+    .then((response) => response);
 };
 
-export const authenticate = data => {
+export const authenticate = (data) => {
   let url = config.remoteIP + "/api/authenticate";
   return fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "x-access-token": sessionStorage.getItem("moda_token")
+      "x-access-token": sessionStorage.getItem("moda_token"),
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
   })
-    .then(response => response.json())
-    .then(response => response);
+    .then((response) => response.json())
+    .then((response) => response);
 };
 
-export const checkToken = token => {
+export const checkToken = (token) => {
   return fetch(config.remoteIP + "/api/checkToken", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-      "x-access-token": token
-    }
+      "x-access-token": token,
+    },
   })
-    .then(response => response.json())
-    .then(response => response);
+    .then((response) => response.json())
+    .then((response) => response);
+};
+
+export const uploadProduct = (data) => {
+  let url = config.remoteIP + "/api/uploadProducts";
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      "x-access-token": sessionStorage.getItem("moda_token"),
+    },
+    body: data,
+  }).then((response) => response);
 };
