42aeb7397c07b508680f16043ce23795f897ea8e
Wylan Osorio <wylanosorio@gmail.com>
2020-05-19 12:53:50 +0800

    modular charts

---
+++ front/src/components/pres/ChartsV2/TotalSalesChart/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {Line} from 'react-chartjs-2';
+
+const TotalSalesChart = ({summaries}) =>{
+    
+    const chartData = {
+        labels: summaries.map((x,i) => i),
+        datasets: [
+          {
+            label: 'Total Sales',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: summaries.map(x => x.sales)
+          }
+        ]
+      };
+    return(
+        <Line data={chartData} />
+    );
+
+}
+
+export default TotalSalesChart;
--- front/src/components/pres/ChartsV2/index.js
+++ front/src/components/pres/ChartsV2/index.js
@@ -2,48 +2,24 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import {Line} from 'react-chartjs-2';
 import s from "./style.module.scss";
+import TotalSalesChart from "./TotalSalesChart";
 
 const ChartsV2 = (report) =>{
     
     let periods = report.report.data.periods;
     periods = periods.slice(0, periods.length - 1);
-    const allSummaries = periods
+
+    const summaries = periods
       .map((d) => ({
         ...d.summary
       }))
       .reverse();
       
-    const data = {
-        labels: allSummaries.map((x,i) => i),
-        datasets: [
-          {
-            label: 'Total Sales',
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: 'rgba(75,192,192,0.4)',
-            borderColor: 'rgba(75,192,192,1)',
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: 'rgba(75,192,192,1)',
-            pointBackgroundColor: '#fff',
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-            pointHoverBorderColor: 'rgba(220,220,220,1)',
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: allSummaries.map(x => x.sales)
-          }
-        ]
-      };
     return(
         <div className={s.canvas}>
         <Grid container>
           <Grid item xs={4}>
-            <Line data={data} />
+            <TotalSalesChart summaries={summaries} />
           </Grid>
         </Grid>
         </div>
