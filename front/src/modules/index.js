b23cc54d7a487987b337ae407ab9c4ad130e6361
Wylan Osorio <wylanosorio@gmail.com>
2020-07-15 20:46:50 +0800

    added dashboard reducer into the root reducer

--- front/src/modules/index.js
+++ front/src/modules/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import sales from './sales';
 import brands from './brands';
 import users from './users';
+import dashboard from './dashboardReducer';
 
 export default combineReducers({
     sales,
     brands,
-    users
+    users,
+    dashboard
 });
